114ef2ad26b15b63b09e79be58da8c51b418e857
MonikaNov <44343981+MonikaNov@users.noreply.github.com>
2018-11-26 14:39:06 +0000

    work

---
+++ Rcodes/NovemberNewVars/ggeffectsLearning.R
@@ -0,0 +1,36 @@
+rm(list=ls())
+library(ggeffects);library(dplyr); library(tseries); library(plm); library(nlme); library(lme4); library(lattice); library(car); library(lmerTest); library(optimx)
+load("dataFS/Main/DaTS.RData")
+
+#oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+# these models are the best ones for now:
+
+KEN11a<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+            +AvgTemp + SDTemp, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+            data=ScaledTS,na.action=na.exclude); summary(KEN11a)
+# and subsamples
+KEN11a_ASAL<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+                 +AvgTemp + SDTemp, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+                 data=ScaledTS[ScaledTS$ASAL==1,],na.action=na.exclude); summary(KEN11a_ASAL)
+
+KEN11a_nonASAL<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+                    +AvgTemp + SDTemp, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+                    data=ScaledTS[ScaledTS$ASAL==0,],na.action=na.exclude); summary(KEN11a_nonASAL)
+
+#---------------------------------------------------------------------------------
+test<-ggeffect(KEN11a)
+test
+exp(summary(KEN11a)$tTable[,1]);ranef(KEN11a)[1]
+
+# and subsamples
+
+test2<-ggpredict(KEN11a)
+test2
+
+
+
+KenTest<-lme(Yield~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+                  +AvgTemp + SDTemp, random= ~1 | ID1,data=ScaledTS,na.action=na.exclude); summary(KenTest); summary(KenTest)$tTable[,1]; ranef(KenTest)
+
+test<-ggeffect(KenTest)
+test
