d40913802079b63a473669732f1558b940c505d9
MonikaNov <44343981+MonikaNov@users.noreply.github.com>
2018-11-27 17:09:38 +0000

    Update ggeffectsLearning.R

--- Rcodes/NovemberNewVars/ggeffectsLearning.R
+++ Rcodes/NovemberNewVars/ggeffectsLearning.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-library(ggeffects);library(dplyr); library(tseries); library(plm); library(nlme); library(lme4); library(lattice); library(car); library(lmerTest); library(optimx)
+library(ggeffects);library(ggplot2);library(dplyr); library(tseries); library(plm); library(nlme); library(lme4); library(lattice); library(car); library(lmerTest); library(optimx)
 load("dataFS/Main/DaTS.RData")
 
 #oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
@@ -52,8 +52,13 @@ efe<-ggeffect(KenTe,terms=c("AvgTemp","ID1"));efe
 
 KEN11a<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
             +AvgTemp + SDTemp, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
-            
+            data=ScaledTS,na.action=na.exclude); summary(KEN11a)
+exp(summary(KEN11a)$tTable[,1])
+               
 kene1<-ggpredict(KEN11a);
 kene2<-ggpredict(KEN11a,terms=c("AvgTemp")); kene2
+kene2b<-ggpredict(KEN11a,terms=c("AvgTemp [all]")); kene2b
+kene2<-ggpredict(KEN11a,terms=c("AvgTemp","ID1")); kene2
 kene3<-ggeffect(KEN11a,terms=c("AvgTemp")); kene3            
-            data=ScaledTS,na.action=na.exclude); summary(KEN11a)
+      kene3b<-ggeffect(KEN11a,terms=c("AvgTemp [log]")); kene3b  
+plot(kene2)+ylab("Yield")
