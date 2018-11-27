a4249e1e01bdc857f534ce3315036993235630f3
MonikaNov <44343981+MonikaNov@users.noreply.github.com>
2018-11-27 15:49:48 +0000

    Update ggeffectsLearning.R

--- Rcodes/NovemberNewVars/ggeffectsLearning.R
+++ Rcodes/NovemberNewVars/ggeffectsLearning.R
@@ -24,13 +24,32 @@ exp(summary(KEN11a)$tTable[,1]);ranef(KEN11a)[1]
 
 # and subsamples
 
-test2<-ggpredict(KEN11a)
-test2
-
+test2<-ggpredict(KEN11a);test2
+test4<-ggaverage(KEN11a);test4
 
 
 KenTest<-lme(Yield~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
-                  +AvgTemp + SDTemp, random= ~1 | ID1,data=ScaledTS,na.action=na.exclude); summary(KenTest); summary(KenTest)$tTable[,1]; ranef(KenTest)
+                  +AvgTemp + SDTemp, random= ~1 | ID1,data=DaTS,na.action=na.exclude); summary(KenTest); 
+summary(KenTest)$tTable[,1]; ranef(KenTest)
 
-test<-ggeffect(KenTest)
-test
+test1<-ggeffect(KenTest,terms=c("AvgTemp"));test1
+test<-ggpredict(KenTest,terms=c("AvgTemp"));test
+te<-ggpredict(KenTest,terms=c("Spell"));te
+te<-ggpredict(KenTest,terms=c("AvgTemp"));te
+hist(ScaledTS$AvgTemp,40)
+
+KenTe<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+             +AvgTemp + SDTemp, random= ~1 | ID1,data=ScaledTS,na.action=na.exclude); summary(KenTe); 
+exp(summary(KenTe)$tTable[,1]); ranef(KenTe)
+
+ete<-ggpredict(KenTe);ete
+ete<-ggpredict(KenTe,terms=c("AvgTemp"));ete
+efe<-ggeffect(KenTe,terms=c("AvgTemp","ID1"));efe
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+KEN11a<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+            +AvgTemp + SDTemp, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+            data=ScaledTS,na.action=na.exclude); summary(KEN11a)
