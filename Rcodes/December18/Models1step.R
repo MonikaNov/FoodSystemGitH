8d5ef8986f9dd54e11a8f8e868734ad192872448
MonikaNov <44343981+MonikaNov@users.noreply.github.com>
2018-12-13 21:50:00 +0000

    working AIC models ecetera

---
+++ Rcodes/December18/Models1step.R
@@ -0,0 +1,52 @@
+rm(list=ls())
+library(dplyr); library(tseries); library(plm); library(nlme); library(lme4); library(lattice); library(car); library(lmerTest); library(optimx)
+load("dataFS/Main/DaTS.RData")
+
+#oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+# these models are the best ones for now:
+
+KEN11a<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+            +AvgTemp + SDTemp, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+            data=ScaledTS,na.action=na.exclude); summary(KEN11a); exp(summary(KEN11a)$coef[[1]])
+
+fullish<-lmer(log(Yield0)~SeasPr+AvgTemp+I(SeasPr^2)+ Prec2m+CVPrec+Spell+Spell4 +MaxP
+                + CVTempK + DDays +(1|ID1),data=ScaledTS) #
+
+Kalinda<-step(fullish, ddf = c("Satterthwaite"), keep=attr(terms(fullish), "term.labels")[1:3])
+summary(get_model(Kalinda))  # ehm..unexpectedly fast...
+
+Cary<-step(fullish, ddf = c("Satterthwaite"), keep=attr(terms(fullish), "term.labels")[1:2])
+summary(get_model(Cary))
+
+drop1(fullish)
+summary(get_model(Kalinda))
+summary(get_model(Cary))
+extractAIC(get_model(Cary));extractAIC(get_model(Kalinda)) # WOW, Kalinda lower AIC here
+
+#oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+#oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+
+full<-lmer(log(Yield0)~SeasPr+AvgTemp+I(SeasPr^2)+ Prec2m+CVPrec+Spell+Spell4 +MaxP
+              + CVTempK + DDays +HWDays+MaxT+(1|ID1),data=ScaledTS) #
+
+Cary2<-step(full, ddf = c("Satterthwaite"), keep=attr(terms(full), "term.labels")[1:3])
+summary(get_model(Cary2))   ;extractAIC(get_model(Cary2));extractAIC(get_model(Kalinda))
+
+Cary3<-step(full, ddf = c("Satterthwaite"), keep=attr(terms(full), "term.labels")[1:2])
+summary(get_model(Cary3))   ;extractAIC(get_model(Cary3));extractAIC(get_model(Kalinda))
+
+#------------------------------------------------------
+
+full<-lmer(log(Yield0)~SeasPr+AvgTemp+I(SeasPr^2)+ Prec2m+CVPrec+Spell+Spell4 +MaxP
+           + CVTempK + DDays +HWDays+(1|ID1),data=ScaledTS) #
+
+Cary2<-step(full, ddf = c("Satterthwaite"), keep=attr(terms(full), "term.labels")[1:3])
+summary(get_model(Cary2))  ;extractAIC(get_model(Cary2));extractAIC(get_model(Kalinda))
+
+Cary3<-step(full, ddf = c("Satterthwaite"), keep=attr(terms(full), "term.labels")[1:2])
+summary(get_model(Cary3))  ;extractAIC(get_model(Cary3));extractAIC(get_model(Kalinda))
+#oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+#oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+
+
+save.image("Rcodes/December18/Models1step.RData")
