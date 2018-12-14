96d191641dfe86dcb636ad3baac9e511e5df2c65
MonikaNov <44343981+MonikaNov@users.noreply.github.com>
2018-12-14 17:26:53 +0000

    working
    playing with the odd sign of CV of temperature

---
+++ Rcodes/December18/KelvinTestModels2.R
@@ -0,0 +1,25 @@
+rm(list=ls())
+library(dplyr); library(tseries); library(plm); library(nlme); library(lme4); library(lattice); library(car); library(lmerTest); library(optimx)
+load("dataFS/Main/DaTS.RData")
+
+#oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+# these models are the best ones for now:
+
+KEN11d<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+            +AvgTemp + CVTempK, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+            data=ScaledTS,na.action=na.exclude); summary(KEN11d); exp(summary(KEN11d)$coef[[1]])
+KEN11h<-update(KEN11d,.~.,method="ML");extractAIC(KEN11h);anova(KEN11g,KEN11h) 
+
+# and subsamples
+KEN11d_ASAL<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+                 +AvgTemp + CVTempK, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+                 data=ScaledTS[ScaledTS$ASAL==1,],na.action=na.exclude); summary(KEN11d_ASAL); exp(summary(KEN11d_ASAL)$coef[[1]])
+
+KEN11d_nonASAL<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+                    +AvgTemp + CVTempK, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+                    data=ScaledTS[ScaledTS$ASAL==0,],na.action=na.exclude); summary(KEN11d_nonASAL); exp(summary(KEN11d_nonASAL)$coef[[1]])
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+KEN11i<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+            +AvgTemp + CVTempK+I(CVTempK*AvgTemp), random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+            data=ScaledTS,na.action=na.exclude); summary(KEN11i); exp(summary(KEN11i)$coef[[1]])
+vif(KEN11i)
---
+++ Rcodes/December18/various.R
@@ -0,0 +1,7 @@
+KEN11j<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4 +AvgTemp+I(AvgTemp^2) + CVTempK,
+            random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+            data=ScaledTS,na.action=na.exclude); summary(KEN11j);
+
+KEN11jML<-update(KEN11j,.~.,method="ML")
+extractAIC(KEN11jML)
+anova(KEN11jML,KEN11h)
