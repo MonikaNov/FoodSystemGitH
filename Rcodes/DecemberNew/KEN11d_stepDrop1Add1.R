a7066c1b0ee9a93058f2b746acdac4edd7ad9719
MonikaNov <44343981+MonikaNov@users.noreply.github.com>
2018-12-17 20:12:40 +0000

    work drop1 add1

--- Rcodes/DecemberNew/KEN11d_stepDrop1Add1.R
+++ Rcodes/DecemberNew/KEN11d_stepDrop1Add1.R
@@ -7,4 +7,34 @@ load("dataFS/Main/DaTS.RData")
 
 KEN11d<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
             +AvgTemp + CVTempK, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
-            data=ScaledTS,na.action=na.exclude); summary(KEN11a); exp(summary(KEN11a)$coef[[1]])
+            data=ScaledTS,na.action=na.exclude); summary(KEN11d); exp(summary(KEN11d)$coef[[1]])
+#oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+# drop1 undefined for restricted ML, have to refit with ML
+
+KEN11d_ML<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+            +AvgTemp + CVTempK, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+            data=ScaledTS,na.action=na.exclude,method="ML"); summary(KEN11d_ML); exp(summary(KEN11d_ML)$coef[[1]])
+
+drop1_KEN11d_ML<-drop1(KEN11d_ML)
+
+scopefullish=formula(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+                     +AvgTemp + CVTempK+I(AvgTemp^2)+ Prec2m +MaxP + DDays,data=ScaleTS)
+
+add1_KEN11d_ML<-add1(KEN11d_ML,scope=scopefullish)
+
+# what about drop1 and lmer?
+KENd_lmer<-lmer(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4+AvgTemp + CVTempK+(1 | ID1),data=ScaledTS,na.action=na.exclude)
+drop1(KENd_lmer)
+anova(KENd_lmer)
+add1(KENd_lmer,scope=scopefullish)
+
+#----  ---  --   ----      -----     -----    ---  ----   ----       ----      ------        ---- -- - - - - -   - -- - - - - -- - - - - -- - - -- - - - -- - - - -- - -- -- - - --- - -- - - - -- - - 
+# now add1 full..
+scopefull=formula(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+                     +AvgTemp + CVTempK+I(AvgTemp^2)+ Prec2m +MaxP + DDays+HWDays+MaxT,data=ScaleTS)
+
+add1_KEN11d_ML2<-add1(KEN11d_ML,scope=scopefull)
+add1_KEN11d_ML2
+add1(KENd_lmer,scope=scopefull)
+#oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+save.image("Rcodes/DecemberNew/KEN11d_stepDrop1Add1.RData")
--- Rcodes/DecemberNew/KEN11d_stepNice.R
+++ Rcodes/DecemberNew/KEN11d_stepNice.R
@@ -76,9 +76,17 @@ CaryML_stepAIC_re2<-stepAIC(fullML_re2)
 myoption <- lmeControl(opt='optim')
 
 fullML_re2<-lme(log(Yield0)~SeasPr+AvgTemp+I(SeasPr^2)+CVPrec+Spell+Spell4 +MaxP
-            + CVTempK, random= ~1+SeasPr+AvgTemp+Spell+Spell4 +MaxP| ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),method="ML",
+            + CVTempK, random= ~1+SeasPr+AvgTemp+Spell+MaxP| ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),method="ML",
             data=ScaledTS,na.action=na.exclude,control=lmeControl(maxIter=300,msMaxIter=300,msMaxEval=500)); summary(fullML_re2); exp(summary(fullML_re2)$coef[[1]])
 CaryML_stepAIC_re2<-stepAIC(fullML_re2)
+
+
+fullML_re3<-lme(log(Yield0)~SeasPr+AvgTemp+I(SeasPr^2)+CVPrec+Spell+Spell4 +MaxP
+            + CVTempK + DDays, random= ~1+SeasPr+AvgTemp+I(SeasPr^2)+CVPrec+Spell+Spell4 +MaxP
+            + CVTempK + DDays| ID1,method="ML",
+            data=ScaledTS,na.action=na.exclude,control=lmeControl(maxIter=300,msMaxIter=300,msMaxEval=500)); summary(fullML_re3); exp(summary(fullML_re3)$coef[[1]])
+CaryML_stepAIC_re3<-stepAIC(fullML_re3)
+
 
 save.image("\\\\smbhome.uscs.susx.ac.uk\\mn301\\FoodSystemGitH\\Rcodes\\DecemberNew\\KEN11d_stepNice3.RData")
 
