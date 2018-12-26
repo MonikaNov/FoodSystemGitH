00c3a02b5ea41168ff3e66d9fc2cc539817d9947
Monika Novackova <monika.novac@gmail.com>
2018-12-27 00:09:58 +0100

    writing

--- Rcodes/December18/playing.R
+++ Rcodes/December18/playing.R
@@ -1,2 +1,14 @@
 aa<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
             +AvgTemp + CVTempK, random= ~1 | ID1 
+        
+        KEN11d<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+                    +AvgTemp + CVTempK, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+                    data=ScaledTS,na.action=na.exclude); summary(KEN11d); exp(summary(KEN11d)$coef[[1]])
+        
+        extractAIC(update(KEN11d,.~.,method="ML"))
+        
+        
+        ff<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+                    +AvgTemp + CVTempK, random= ~CVPrec+1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+                    data=ScaledTS,na.action=na.exclude,method="ML"); summary(ff)
+        anova(ff,update(KEN11d,.~.,method="ML"))
