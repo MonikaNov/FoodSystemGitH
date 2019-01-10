af4d3c3a1292cb1fd803ff48a554c2a3e884de32
MonikaNov <44343981+MonikaNov@users.noreply.github.com>
2019-01-10 18:42:41 +0000

    trying to work cross-validation, jackknife

--- Rcodes/crossvalidation/first.R
+++ Rcodes/crossvalidation/first.R
@@ -13,12 +13,32 @@ dim(ScaledTS[complete.cases(ScaledTS),])
 
 KEN11dK<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
              +AvgTempK + CVTempK, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
-             data=ScaledTS,na.action=na.exclude); summary(KEN11dK)
+             data=ScaledTS,na.action=na.omit); summary(KEN11dK)
+#trying:
+            foo<-lme(Yield0~Year+ID1+SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+              +AvgTempK + CVTempK, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+               data=ScaledTS,na.action=na.omit); summary(KEN11dK)
 
-CVfit1<-cvFit(KEN11dK,data=ScaledTS,y=log(ScaledTS$Yield0))
-CVfit1
 
-CVfit1<-cvFit(KEN11dK,data=ScaledTS[complete.cases(ScaledTS),],y=log(ScaledTS[complete.cases(ScaledTS),]$Yield0))
-CVfit1
+        framik<-model.frame(foo,data=ScaledTS)[complete.cases(model.frame(foo,data=ScaledTS)),]
+        KEN11dK_fr<-lme(log(Yield0)~SeasPr+I(SeasPr^2)+CVPrec+Spell+Spell4
+                     +AvgTempK + CVTempK, random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+                     data=framik,na.action=na.omit);summary(KEN11dK_fr)
+        
+        
+        CVfit1<-cvFit(KEN11dK,data=ScaledTS,y=log(ScaledTS$Yield0))
+        CVfit1<-cvFit(KEN11dK,data=ScaledTS[complete.cases(ScaledTS),])
+        CVfit1<-cvFit(KEN11dK_fr,data=framik)
+        
+CVfit2<-cvFit(KEN11dK,data=ScaledTS[complete.cases(ScaledTS),],y=log(ScaledTS[complete.cases(ScaledTS),]$Yield0))
+CVfit2;summary(CVfit2)
 
-summary(CVfit1)
+CVfit3<-cvFit(KEN11dK,data=framik,y=log(framik$Yield0));CVfit3
+CVfit4<-cvFit(KEN11dK,data=framik,y=log(framik$Yield0),K=1300);CVfit4
+#--  ---   --   --- -- - - -
+predict(KEN11dK); summary(predict(KEN11dK)); length(predict(KEN11dK))
+predict(KEN11dK,asList=TRUE); summary(unlist(predict(KEN11dK,asList=TRUE)))
+summary(CVfit3)
+
+# -- -- -- -- -
+# ok, I will try to obtain the same prediction as the function predict...
