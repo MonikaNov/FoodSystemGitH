15708ae4132212035efa1898f7d96caaa5c52a0b
Monika Novackova <monika.novac@gmail.com>
2019-01-13 19:13:59 +0000

    working cross validation comparison with the seasonal total models

--- Rcodes/crossvalidation/compareJustTotals.R
+++ Rcodes/crossvalidation/compareJustTotals.R
@@ -44,7 +44,35 @@ KEN11dK_base<-lme(log(Yield0)~SeasPr+AvgTempK, random= ~1 | ID1,correlation=corA
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 CVbase1<-cvFit(KEN11dK_base,data=framik,y=log(framik$Yield0),K=1300);CVbase1;summary(CVbase1)
 CVbase2<-cvFit(KEN11dK_base,data=framik,y=log(framik$Yield0),K=1300,cost=rtmspe);CVbase2;summary(CVbase2)
+CVbase3<-cvFit(KEN11dK_base,data=framik,y=log(framik$Yield0),K=1300,cost=mape);CVbase3;summary(CVbase3)
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+KEN11dK_base2<-lme(log(Yield0)~SeasPr+AvgTempK+I(SeasPr^2), random= ~1 | ID1,correlation=corARMA(form = ~ as.numeric(Year)|ID1, p=1,q=1),
+                  data=ScaledTS,na.action=na.omit); summary(KEN11dK_base2)
 
+CVbase21<-cvFit(KEN11dK_base2,data=framik,y=log(framik$Yield0),K=1300);CVbase21;summary(CVbase21)
+CVbase22<-cvFit(KEN11dK_base2,data=framik,y=log(framik$Yield0),K=1300,cost=rtmspe);CVbase22;summary(CVbase22)
+CVbase23<-cvFit(KEN11dK_base2,data=framik,y=log(framik$Yield0),K=1300,cost=mape);CVbase23;summary(CVbase23)
+
+save.image("~/FoodSystemGitH/Rcodes/crossvalidation/compareJustTotals2.RData")
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+KEN11dK_base3<-lme(log(Yield0)~SeasPr+AvgTempK, random= ~1 | ID1,
+                  data=ScaledTS,na.action=na.omit); summary(KEN11dK_base3)
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+CVbase31<-cvFit(KEN11dK_base3,data=framik,y=log(framik$Yield0),K=1300);CVbase31;summary(CVbase31)
+CVbase32<-cvFit(KEN11dK_base3,data=framik,y=log(framik$Yield0),K=1300,cost=rtmspe);CVbase32;summary(CVbase32)
+CVbase33<-cvFit(KEN11dK_base3,data=framik,y=log(framik$Yield0),K=1300,cost=mape);CVbase33;summary(CVbase33)
+
+save.image("~/FoodSystemGitH/Rcodes/crossvalidation/compareJustTotals2.RData")
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+KEN11dK_base4<-lme(log(Yield0)~SeasPr+AvgTempK+I(SeasPr^2), random= ~1 | ID1,
+                   data=ScaledTS,na.action=na.omit); summary(KEN11dK_base4)
+
+CVbase41<-cvFit(KEN11dK_base4,data=framik,y=log(framik$Yield0),K=1300);CVbase41;summary(CVbase41)
+CVbase42<-cvFit(KEN11dK_base4,data=framik,y=log(framik$Yield0),K=1300,cost=rtmspe);CVbase42;summary(CVbase422)
+CVbase43<-cvFit(KEN11dK_base4,data=framik,y=log(framik$Yield0),K=1300,cost=mape);CVbase43;summary(CVbase43)
+
+save.image("~/FoodSystemGitH/Rcodes/crossvalidation/compareJustTotals3.RData")
 #oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
 
 save.image("~/FoodSystemGitH/Rcodes/crossvalidation/compareJustTotals.RData")
